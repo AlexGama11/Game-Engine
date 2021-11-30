5f4692c3d495b0de331785a608e9397a778abc5c
Alex Gama <alexgama1612@gmail.com>
2021-11-30 00:23:29 +0000

    added comments

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,25 +8,30 @@ bool isGameRunning = true;
 
 int main(int argc, char* argv[])
 {
+	//initializes
 	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
 	{
 		std::cout << "SDL did not Initialize!" << std::endl;
 		return -1;
 	}
 	
+	//makes window
 	window = SDL_CreateWindow("Alex's Game Engine",
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
 		1280, 720,
 		0);
 
+	//if no window
 	if (!window)
 	{
 		std::cout << "Game Window could not be created!" << std::endl;
 	}
 
+	// makes renderer
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+	//if no renderer
 	if (!renderer)
 	{
 		std::cout << "Renderer could not be created!" << std::endl;
@@ -42,6 +47,7 @@ int main(int argc, char* argv[])
 		//Swaps the Frame buffer
 		SDL_RenderPresent(renderer);
 	}
+
 
 	//Shutdown Game
 	SDL_DestroyRenderer(renderer);
