041c037642f2e2e90c75808349da250284c4d841
Alex Gama <alexgama1612@gmail.com>
2021-12-01 02:38:50 +0000

    Input code in its class and made main work

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,57 +1,32 @@
 #include <iostream>
 #include <SDL.h>
-
-SDL_Window* window = nullptr;
-SDL_Renderer* renderer = nullptr;
+#include "Screen.h"
 
 bool isGameRunning = true;
 
 int main(int argc, char* argv[])
 {
-	//initializes
-	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
-	{
-		std::cout << "SDL did not Initialize!" << std::endl;
-		return -1;
-	}
+	// initializes the game
+	Screen Init;
+	Init.Initialize();
 	
-	//makes window
-	window = SDL_CreateWindow("Alex's Game Engine",
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		1280, 720,
-		0);
-
-	//if no window
-	if (!window)
-	{
-		std::cout << "Game Window could not be created!" << std::endl;
-	}
-
-	// makes renderer
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-	//if no renderer
-	if (!renderer)
-	{
-		std::cout << "Renderer could not be created!" << std::endl;
-	}
+	
+	// makes the screen
+	Screen Presenter;
+	Presenter.Present();
 
 	// Main Game Loop
 	while (isGameRunning)
 
 	{
-		//Clears the Screen
-		SDL_RenderClear(renderer);
-
-		//Swaps the Frame buffer
-		SDL_RenderPresent(renderer);
+		//refreshes the screen
+		Screen Refresher;
+		Refresher.Refresh();
 	}
 
 
-	//Shutdown Game
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit;
+	// Shuts down the game
+	Screen Quitting;
+	Quitting.Shutdown();
 	return 0;
 }
--- src/Screen.cpp
+++ src/Screen.cpp
@@ -1,18 +1,66 @@
+#include <iostream>
+#include <SDL.h>
 #include "Screen.h"
+
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
 
 bool Screen::Initialize()
 {
+
+	//initializes
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
+	{
+		std::cout << "SDL did not Initialize!" << std::endl;
+		return -1;
+	}
+
     return false;
 }
 
 void Screen::Refresh()
 {
+
+	//Clears the Screen
+	SDL_RenderClear(renderer);
+
+	//Swaps the Frame buffer
+	SDL_RenderPresent(renderer);
+
 }
 
 void Screen::Present()
 {
+
+	//makes window
+	window = SDL_CreateWindow("Alex's Game Engine",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		1280, 720,
+		0);
+
+	//if no window
+	if (!window)
+	{
+		std::cout << "Game Window could not be created!" << std::endl;
+	}
+
+	// makes renderer
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	//if no renderer
+	if (!renderer)
+	{
+		std::cout << "Renderer could not be created!" << std::endl;
+	}
+
 }
 
 void Screen::Shutdown()
 {
+	//shutdown game
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit;
+
 }
